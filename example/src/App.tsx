30277f257ab1011712a32444ff77bac48fbe00df
phelpsandrew <andrew.phelps@msj.edu>
2026-01-09 12:32:41 -0500

    Ran prettier and ensured lint ran successfully

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,20 +2,27 @@ import { View, StyleSheet, Button } from 'react-native';
 import { addEventWithEditor } from 'react-native-events-export';
 
 export default function App() {
-
   const addEvent = () => {
-    try{
-      addEventWithEditor("Test Event", Date.now(), Date.now() + 3600 * 1000, "Test Location");
+    try {
+      addEventWithEditor(
+        'Test Event',
+        Date.now(),
+        Date.now() + 3600 * 1000,
+        'Test Location'
+      );
     } catch {
-      console.log("Something happened");
+      console.log('Something happened');
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
-      <Button title={"Add Event"} onPress={() => {
-        addEvent();
-      }}/>
+      <Button
+        title={'Add Event'}
+        onPress={() => {
+          addEvent();
+        }}
+      />
     </View>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import EventsExport from './NativeEventsExport';
 
-export function addEventWithEditor(title: string, startDate: number, endDate: number, location?: string): Promise<void> {
+export function addEventWithEditor(
+  title: string,
+  startDate: number,
+  endDate: number,
+  location?: string
+): Promise<void> {
   return EventsExport.addEventWithEditor(title, startDate, endDate, location);
 }
